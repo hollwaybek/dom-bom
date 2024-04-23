04fd95440f23e56149e38593757e47b7bbf7f62d
hollwaybek <hollwayuzb@gmail.com>
2024-04-23 13:52:57 +0500

    full

--- main.js
+++ main.js
@@ -20,9 +20,20 @@
 // });
 
 
-function generateRandomNumber() {
-    var randomNumber = Math.floor(Math.random() * 100); 
-    document.getElementById("randomNumber").innerHTML = "Tasodifiy Son: " + randomNumber; 
-}
+// function generateRandomNumber() {
+//     var randomNumber = Math.floor(Math.random() * 100); 
+//     document.getElementById("randomNumber").innerHTML = "Tasodifiy Son: " + randomNumber; 
+// }
 
-window.onload = generateRandomNumber;
+// window.onload = generateRandomNumber;
+
+
+
+let button = document.querySelector(".random_btn")
+let random = document.querySelector(".random__number")
+
+
+button.addEventListener('click', () => {
+    // let newNUm = document.createElement('p')
+    random.textContent = Math.round(Math.random() * 100)
+})
